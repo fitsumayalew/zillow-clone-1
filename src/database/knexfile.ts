2c0b1f5e6e8bf182a68ad82e9b7d33c61327b71a
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-09 12:36:29 +0300

    (fix) removed database pools for heroku

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -26,10 +26,6 @@ export = {
   staging: {
     client: "postgresql",
     connection: DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
     migrations: {
       tableName: "knex_migrations",
     },
