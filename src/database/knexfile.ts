0b7a103ec5998a3d9d09808ac9e295c1a5c52476
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-09 13:25:44 +0300

    (fix) added lines to debug heroku config

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -33,10 +33,6 @@ export = {
   production: {
     client: "postgresql",
     connection: DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
     migrations: {
       tableName: "knex_migrations",
     },
