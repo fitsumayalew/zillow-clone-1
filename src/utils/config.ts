7d5ffc77015375c4c1dc58240fa0c4b9db975fab
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-27 13:02:44 +0300

    (refactor) Added comments

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,11 +8,12 @@ const CLOUDINARY_FOLDER = process.env.CLOUDINARY_FOLDER || "zillow";
 const DATABASE_URL = process.env.DATABASE_URL;
 const JWT_SECRET = process.env.JWT_SECRET || "";
 const NODE_ENV = process.env.NODE_ENV || "production";
-<<<<<<< HEAD
-=======
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY || "";
 const CLOUDINARY_USER_URL = process.env.CLOUDINARY_USER_URL || "";
->>>>>>> f6de304... (refactor) Added code comments
+const SALT_ROUNDS = 12;
+const JWT_RESET_PASSWORD_SECRET = process.env.JWT_RESET_PASSWORD_SECRET || "";
+const JWT_RESET_PASSWORD_LIFE_TIME =
+  process.env.JWT_RESET_PASSWORD_LIFE_TIME || "";
 
 export default {
   DATABASE_URL,
@@ -22,9 +23,9 @@ export default {
   JWT_SECRET,
   JWT_LIFE_TIME,
   CLOUDINARY_FOLDER,
-<<<<<<< HEAD
-=======
   GOOGLE_MAPS_API_KEY,
   CLOUDINARY_USER_URL,
->>>>>>> f6de304... (refactor) Added code comments
+  SALT_ROUNDS,
+  JWT_RESET_PASSWORD_SECRET,
+  JWT_RESET_PASSWORD_LIFE_TIME,
 };
